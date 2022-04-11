2e4f0dbfd225f1adc0ac5cef9e1e356af38c4ebd
KovalchukV1 <kovalchukvv001@gmail.com>
2022-04-11 17:09:18 +0300

    findElement

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,4 +1,5 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
@@ -9,5 +10,6 @@ public class Tests extends Home1 {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.get("https://eatstreet.com/");
+        driver.findElement(By.xpath("//div[@aria-pressed='false']")).click();
     }
 }
