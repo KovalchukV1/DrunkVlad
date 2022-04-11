3a9bc57250ae51ae79431e2d8a7abb262d59eba0
KovalchukV1 <kovalchukvv001@gmail.com>
2022-04-11 16:09:57 +0300

    Install pom.xml

--- src/test/java/Home1.java
+++ src/test/java/Home1.java
@@ -1,3 +1,16 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class Home1 {
+WebDriver driver;
+
+        @Test
+    public void EatStreat(){
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+            driver.get("https://eatstreet.com/");
+        }
 
 }
