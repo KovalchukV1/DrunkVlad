d87bc38b68b0e1625d9732e24885dc196060f7de
KovalchukV1 <kovalchukvv001@gmail.com>
2022-04-12 11:53:15 +0300

    2th test

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,9 +1,19 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Tests extends Home1 {
+
+    public void sleep(int seconds) {
+        try {
+            Thread.sleep(seconds * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Test
     public void EatStreat(){
@@ -11,5 +21,22 @@ public class Tests extends Home1 {
         driver = new ChromeDriver();
         driver.get("https://eatstreet.com/");
         driver.findElement(By.xpath("//div[@aria-pressed='false']")).click();
+    }
+
+    @Test
+    public void testIntertop() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://intertop.ua/ua/");
+        driver.manage().window().maximize();
+        driver.findElement(By.xpath("//div[@class='auth-ico']")).click();
+        sleep(2);
+        driver.findElement(By.xpath("//div/input[@id='email']")).sendKeys("lalokyrbi@gmail.com");
+        driver.findElement(By.xpath("//div/input[@id='password']")).sendKeys("blabla");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        sleep(2);
+        Assert.assertEquals(driver.findElements(By.xpath("//div[contains(text(), 'Невірний логін або пароль')]")).size(),2);
+        driver.quit();
+
     }
 }
