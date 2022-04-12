7d79f58831079b16962b9ee0c9a995a6ac0df506
KovalchukV1 <kovalchukvv001@gmail.com>
2022-04-12 12:11:44 +0300

    Create BeforeMethod

--- src/test/java/Home1.java
+++ src/test/java/Home1.java
@@ -1,10 +1,26 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class Home1 {
 WebDriver driver;
+
+    public void sleep(int seconds) {
+        try {
+            Thread.sleep(seconds * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @BeforeMethod
+    public void beforeMrthod(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
 
 
 }
--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -7,25 +7,17 @@ import org.testng.annotations.Test;
 
 public class Tests extends Home1 {
 
-    public void sleep(int seconds) {
-        try {
-            Thread.sleep(seconds * 1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
 
     @Test
     public void EatStreat(){
-        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
         driver.get("https://eatstreet.com/");
         driver.findElement(By.xpath("//div[@aria-pressed='false']")).click();
     }
 
     @Test
     public void testIntertop() {
-        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.get("https://intertop.ua/ua/");
         driver.manage().window().maximize();
@@ -37,6 +29,7 @@ public class Tests extends Home1 {
         sleep(2);
         Assert.assertEquals(driver.findElements(By.xpath("//div[contains(text(), 'Невірний логін або пароль')]")).size(),2);
         driver.quit();
-
     }
+
+
 }
